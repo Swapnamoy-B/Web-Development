fb48fc2735ce75068c9234e4b061cb4395d2fae3
Swapnamoy Bhattacharjee <111850220+Swapnamoy-B@users.noreply.github.com>
2022-10-11 10:25:12 +0530

    Add files via upload

---
+++ News-Letter/app.js
@@ -0,0 +1,63 @@
+const express =  require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended :true}));
+
+app.get("/",function (req,res) {
+  res.sendFile(__dirname + "/signup.html")
+});
+
+app.post("/",function(req,res){
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members :[
+      {
+        email_address : email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME : firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = 'https://us9.api.mailchimp.com/3.0/lists/4c8844675c'
+  const options = {
+    method : "POST",
+    auth:"Swapnamoy:3a0e4958368df4602ba74c27d6f2810e-us9"
+  }
+  const request = https.request(url, options, function(response) {
+    if(response.statusCode===200){
+      res.sendFile(__dirname + "/success.html");
+    }else{
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function(req,res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000 ")
+})
+
+//4c8844675c
+// 3a0e4958368df4602ba74c27d6f2810e-us9
